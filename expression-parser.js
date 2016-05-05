f3db5fcaab60d953e6167f00d4b45a7b00aacaf2
ehjay <ehjay@users.noreply.github.com>
2016-05-05 15:37:42 -0400

    parse a token stream

--- expression-parser.js
+++ expression-parser.js
@@ -3,7 +3,38 @@ module.exports = (function() {
     toTokenStream: toTokenStream
   }
 
-  function toTokenStream() {
-    return true;
+  function getToken(stream, source) {
+    var rx_token = /^((\s+)|([a-zA-Z][a-zA-Z0-9_]*)|(\()|(\))|(!=|==|<=|>=|<|>)|(-|\+)|(\*|\/|,))(.*)$/;
+
+    // 0 source
+    // 1 match
+    // 2 whitespace
+    // 3 identifier
+    // 4 open bracket
+    // 5 close bracket
+    // 6 relational operators
+    // 7 unary or binary operators (+ or -)
+    // 8 binary operators (* or / or ,)
+    // 9 the rest
+
+    var results = source.match(rx_token);
+    var value = results[1];
+    var rest = results[9];
+    stream.push(value);
+
+    return rest;
+  }
+
+  function toTokenStream(source) {
+    var stream = [];
+    var safety = 0;
+    var len = source.length;
+
+    while (source.length > 0 && safety < len) {
+      source = getToken(stream, source);
+      safety++;
+    }
+
+    return stream;
   }
 })();
--- expression-parser.test.js
+++ expression-parser.test.js
@@ -3,8 +3,13 @@ var parser = require('./expression-parser');
 
 describe('expression-parser', function() {
   describe('#toTokenStream', function () {
-    it('should return true', function () {
-      assert.equal(parser.toTokenStream(), true);
+    it('should return an array', function () {
+      assert.isArray(parser.toTokenStream(""));
+    });
+
+    it('should return a token stream', function () {
+      assert.deepEqual(parser.toTokenStream("a"), ["a"]);
+      assert.deepEqual(parser.toTokenStream("  MAX(a + b, c)"), ["  ","MAX","(","a"," ","+"," ","b",","," ","c",")"]);
     });
   });
 });
