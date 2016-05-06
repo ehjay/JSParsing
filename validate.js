dd8ea023e255e6df36ce8c5b4d4b6aed9e2de880
ehjay <ehjay@users.noreply.github.com>
2016-05-06 15:23:17 -0400

    expand tokens

--- validate.js
+++ validate.js
@@ -1,11 +1,79 @@
-var tokenize = require('./tokenize');
 var _ = require('lodash');
+var stackFactory = require('./stack');
+var tokenize = require('./tokenize');
 
 module.exports = (function() {
-  return validate;
+  return {
+    expandTokens: expandTokens,
+    validate: validate
+  };
 
-  function validate(source) {
-    return {};
+  function validate(source, functionNames, variableNames) {
+    var stack = stackFactory();
+    var tokens = tokenize(source);
+    var valid = true;
+    var warnings = [];
+
+    expandTokens(tokens, functionNames, variableNames, warnings);
+
+    _.forEach(tokens, function(token) {
+      validateToken(token, stack, warnings);
+
+      if (warnings.length > 0) {
+        valid = false;
+        return false;
+      }
+    });
+
+    return {
+      valid: valid,
+      warnings: warnings
+    };
+  }
+
+  function expandTokens(tokens, functionNames, variableNames, warnings) {
+    var column = 1;
+
+    for (var i=0; i < tokens.length; i++) {
+      var token = tokens[i];
+      token.column = column;
+      column += token.value.length;
+
+      if (token.type !== "identifier") {
+        continue;
+      }
+
+      if (_.includes(functionNames, token.value)) {
+        token.type = "function";
+      } else if (_.includes(variableNames), token.value) {
+        token.type = "variable";
+      } else {
+        warnings.push("unknown identifier " + token.value + " found at column " + column);
+      }
+    }
+
+    return tokens;
+  }
+
+  function validateToken(token, stack, warnings) {
+    switch(token.type) {
+      case "whitespace":
+        break;
+      case "function":
+        break;
+      case "variable":
+        break;
+      case "literal":
+        break;
+      case "(":
+        break;
+      case ")":
+        break;
+      case "unary_or_binary":
+        break;
+      case "binary":
+        break;
+    }
   }
 })();
 
--- validate.test.js
+++ validate.test.js
@@ -4,7 +4,59 @@ var validate = require('./validate');
 describe('validate', function() {
   describe('#validate', function () {
     it('should return an object', function () {
-      assert.isObject(validate("source expression"));
+      assert.isObject(validate.validate("source expression", [], []));
+    });
+
+    it('should expand function tokens', function () {
+      assert.deepEqual(
+        validate.expandTokens(
+          [
+            { type: "identifier", value: "MAX" }
+          ],
+          ["MAX"],
+          [],
+          []
+        ),
+        [
+          { column: 1, type: "function", value: "MAX" }
+        ]
+      );
+    });
+
+    it('should expand variable tokens', function () {
+      assert.deepEqual(
+        validate.expandTokens(
+          [
+            { type: "identifier", value: "my_var" }
+          ],
+          [],
+          ["my_var"],
+          []
+        ),
+        [
+          { column: 1, type: "variable", value: "my_var" }
+        ]
+      );
+    });
+
+    it('should count columns', function () {
+      assert.deepEqual(
+        validate.expandTokens(
+          [
+            { type: "identifier", value: "a" },
+            { type: "whitespace", value: " " },
+            { type: "identifier", value: "b" }
+          ],
+          [],
+          ["a", "b"],
+          []
+        ),
+        [
+          { column: 1, type: "variable", value: "a" },
+          { column: 2, type: "whitespace", value: " " },
+          { column: 3, type: "variable", value: "b" }
+        ]
+      );
     });
 
   });
